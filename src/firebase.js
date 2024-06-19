b30ddf227e22f7ab1fcb25d52f470eab414ef8f7
Shaveen12 <126313485+Shaveen12@users.noreply.github.com>
2024-06-19 09:05:19 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,7 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCq8gEoN8dHjDVBBzczpq0jjc9WAbs-T2w",
-  authDomain: "chat-47fd5.firebaseapp.com",
-  projectId: "chat-47fd5",
-  storageBucket: "chat-47fd5.appspot.com",
-  messagingSenderId: "606819201253",
-  appId: "1:606819201253:web:15dbd26f9fd0882963e40c",
-  measurementId: "G-S5T9SLMFZZ"
+  
 };
 
 // Initialize Firebase
